ae95711083328868630b1909ced5fd880beb17b7
rinbo <robin.b@outlook.com>
2018-11-20 23:12:49 +0100

    Fix MuiThemeProvider issue that caused compilation to fail

--- src/containers/FormsContainer.js
+++ src/containers/FormsContainer.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import {CreateCompany} from '../components/CreateCompany'
-import {ListCompany} from '../components/ListCompany'
-import {CreatePerson} from '../components/CreatePerson'
-import {ListPerson} from '../components/ListPerson'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { CreateCompany } from '../components/CreateCompany'
+import { ListCompany } from '../components/ListCompany'
+import { CreatePerson } from '../components/CreatePerson'
+import { ListPerson } from '../components/ListPerson'
 import AppBar from 'material-ui/AppBar/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export class FormsContainer extends Component {
   
