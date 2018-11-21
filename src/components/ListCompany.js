dd50189c17078f6893b6a3f88a89b3fbd159cc9d
rinbo <robin.b@outlook.com>
2018-11-21 11:14:33 +0100

    Import MUI list items to list companies with an icon

--- src/components/ListCompany.js
+++ src/components/ListCompany.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react'
-import Typography from '@material-ui/core/Typography'
+import { Typography, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+import BusinessIcon from '@material-ui/icons/Business'
+
 
 export class ListCompany extends Component {
   
   render() {
 
     const listCompanies = this.props.companies.map((company) => {
-      return <li key={company}>{company}</li>
+      return (       
+          <ListItem>
+            <ListItemIcon>
+              <BusinessIcon />
+            </ListItemIcon>
+            <ListItemText primary={company} />
+          </ListItem>        
+      )
     })
 
     return (
       <div>
         <Typography variant="headline">List Company Employees</Typography>
-        <ul>{listCompanies}</ul>
+        <List>{listCompanies}</List>
       </div>
 
     )
